59b5097f860144aeba4e6222d274f01654352077
patinyasas <patinyasas.auns@bumail.net>
2024-03-20 20:31:16 +0700

    done yay

---
+++ Assets/Scripts/Resources/ResourceManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] woodTreePrefab;
+
+    [SerializeField]
+    private Transform woodTreeParent;
+
+    [SerializeField]
+    private ResourceSource[] resources;
+    public ResourceSource[] Resources
+    {
+        get { return resources; }
+    }
+
+    public static ResourceManager instance;
+
+    private void FindAllResource()
+    {
+        resources = FindObjectsOfType<ResourceSource>();
+    }
+    void Awake()
+    {
+        instance = this;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindAllResource();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/MainUI.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainUI : MonoBehaviour
+{
+    [SerializeField] 
+    private GameObject selectionMarker; 
+    public GameObject SelectionMarker
+    {
+        get { return selectionMarker; }
+    }
+    [SerializeField] private TextMeshProUGUI unitCountText;
+    [SerializeField] private TextMeshProUGUI foodText;
+    [SerializeField] private TextMeshProUGUI woodText;
+    [SerializeField] private TextMeshProUGUI goldText;
+    [SerializeField] private TextMeshProUGUI stoneText;
+    
+    public void UpdateAllResource(Faction faction)
+    {
+        unitCountText.text = faction.AliveUnits.Count.ToString();
+        foodText.text = faction.Food.ToString();
+        woodText.text = faction.Wood.ToString();
+        goldText.text = faction.Gold.ToString();
+        stoneText.text = faction.Stone.ToString();
+    }
+
+    
+    public static MainUI instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
